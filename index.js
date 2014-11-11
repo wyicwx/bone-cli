ccc7fa0a967cd558b42ba58b9445709578621343
wyicwx <wyicwx@gmail.com>
2014-11-11 12:12:33 +0800

    up

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ var path = require('path'),
 	Command = require('commander').Command;
 
 function setupBone(base) {
-	var bone = resolve.sync('bone', {basedir: base});
-	bone = require(bone);
+	try {
+		var bone = resolve.sync('bone', {basedir: base});
+		bone = require(bone);
+	} catch(e) {
+		console.log('Fatal error: Unable to find local bone.');
+		process.exit(0);
+	}
 	bone.commander = new Command('bone');
 	bone.commander.version(bone.version);
 
