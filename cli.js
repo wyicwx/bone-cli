5f986bb212b0bb36081782e3a660e9404c7138fa
wyicwx <wyicwx@gmail.com>
2015-02-10 16:06:57 +0800

    rebuild task, support more feature

--- cli.js
+++ cli.js
@@ -1,33 +1,28 @@
 var Command = require('commander').Command;
 var spawn = require('child_process').spawn;
 var os = require('os');
+var task = require('./task');
 
 exports.setup = function(bone) {
 	var commander = new Command('bone');
-	var commandList = {};
-	var tasks = {};
-	var commanders = {};
+	var commanders = commander.commanders = {};
 
 	commander.on('*', function(args) {
-		var task = args[0];
-		if(task in tasks) {
-			tasks[task]();
-		} else {
+		if(!task.run(args[0])) {
 			commander.outputHelp();
 		}
 	});
 
 	bone.cli = function(module, option) {
 		option || (option = {});
-
+		var cmder;
 		var command = function(name) {
 			var cname = option.alias || name;
 
 			commanders[cname] = {events: []};
 			var subccommander = commanders[cname]['commander'] = commander.command(cname);
 			subccommander.command = null;
-
-			commandList[cname] = subccommander;
+			subccommander.cname = cname;
 
 			if(option.always) {
 				commanders[cname].events.push(option.always);
@@ -38,131 +33,19 @@ exports.setup = function(bone) {
 					fn.call(null);
 				});
 			});
+			cmder = subccommander;
 			return subccommander;
 		};
 
 		module(command, bone);
+
+		return cmder;
 	};
 
-	
-	/**
-		usage:
-			bone.cli.task('release', {
-				name: 'build',
-				params: {
-					project: 'dist'
-				}
-			},
-			{
-				name: 'connect'
-			},
-			{
-				exec: 'node main.js'
-			});
-	 */
-	bone.task = bone.cli.task = function(name) {
-		var cmds = Array.prototype.slice.call(arguments, 1);
-		var runQueue = [];
-		var run = function(cmd, args) {
-			return function(next) {
-				if(os.platform() == 'win32') {
-					args = ['/c', cmd].concat(args);
-					cmd = 'cmd';
-				}
-				var child = spawn(cmd, args, {
-					stdio: [0, 1, 2], 
-					env: bone.utils.extend({BONE_TASK: true}, process.env),
-					encoding: 'utf8'
-				});
-				child.on('exit', function(code) {
-					if(code == 0) {
-						next();
-					} else {
-						console.log('Failed : '+cmd+' '+args);
-					}
-				});
-			}
-		}
-		cmds.forEach(function(option) {
-			if(typeof option === 'function') {
-				return runQueue.push(option);
-			}
-			if(typeof option === 'string') {
-				if(option in tasks || option in commanders) {
-					option = {name: option};
-				} else {
-					option = {exec: option};
-				}
-			}
-			if(option.exec) {
-				var args = option.exec.split(' ').splice(1);
-				var cmd = option.exec.split(' ')[0];
-				runQueue.push(run(cmd, args));
-			} else if(option.name) {
-				if(option.name in tasks || option.name in commanders) {
-					var args = [option.name];
-					for(var i in option.params) {
-						args.push((i.length > 1 ? '--' : '-') + i);
-						if(option.params[i]) {
-							args.push(option.params[i]);
-						}
-					}
-					if(option.always) {
-						commanders[option.name].events.push(function() {
-							if(process.env.BONE_TASK) {
-								option.always.call(null);
-							}
-						});
-					}
-					runQueue.push(run('bone', args));
-				}
-			}
-		});
-
-		if(!runQueue.length) {
-			return;
-		}
-
-		tasks[name] = function() {
-			var index = -1;
-			function next() {
-				index++;
-				var runFn = runQueue[index];
-				if(runFn) {
-					if(runFn.length) {
-						runFn(next);
-					} else {
-						runFn();
-						next();
-					}
-				}
-			}
-			next();
-		};
-
-		var usage = ['[options] [command]\r\n\r\n  Task:'];
-		for(var i in tasks) {
-			usage.push(i);
-		}
-		commander.usage(usage.join(' '));
-	};
-
-	bone.cli.log = function() {
-
-	};
-
-	bone.cli.error = function() {
-
-	};
-
-	bone.cli.warnning = function() {
-
-	};
-
+	task.setup(bone, commander, commanders);
 	commander.version(bone.version);
 	var build = require('bone-build');
 	bone.cli(build());
 
 	return commander;
-}
-
+};
---
+++ task.js
@@ -0,0 +1,200 @@
+var bone;
+var commander;
+var os = require('os');
+var spawn = require('child_process').spawn;
+var debug = function() {};
+
+function _cp(cmd, option, callback) {
+	var args = option.args || [];
+	var env = option.env || {};
+
+	debug('start run spawn.'.green);
+	debug('cmd:', cmd);
+	debug('args:', args);
+
+	if(cmd === 'bone') {
+		args = [];
+		env.BONE_ARGV = args.join(' ');
+	}
+	if(os.platform() == 'win32') {
+		args = ['/c', cmd].concat(args);
+		cmd = 'cmd';
+	}
+
+	spawn(cmd, args, {
+		stdio: [0, 1, 2], 
+		env: bone.utils.extend(env, process.env),
+		encoding: 'utf8'
+	}).on('exit', function(code) {
+		if(code == 0) {
+			callback(null);
+		} else {
+			console.log('Fatal error: '+cmd+' '+args);
+		}
+	});
+}
+/**
+ * {
+ * 	exec: 'grunt debug'
+ * }
+ * {
+ * 	name: 'build',
+ * 	params: {
+ * 		_: 'file'
+ * 		p: 'release'
+ * 	}
+ * }
+ */
+function _parse(work) {
+	var tasks = Task.tasks;
+	var commanders = commander.commanders;
+	var parsedWork = {
+		inBone: true
+	};
+	if(typeof work === 'function') {
+		parsedWork.beforeRun = work;
+	} else if(typeof work === 'string') {
+		parsedWork.args = work.split(' ');
+		parsedWork.exec = parsedWork.args.shift();
+	} else {
+		work = bone.utils.pick(work, ['name', 'exec', 'params', 'always', 'cli']);
+		bone.utils.extend(parsedWork, work);
+		if(!parsedWork.exec && parsedWork.name) {
+			parsedWork.exec = parsedWork.name;
+		} else if(!parsedWork.name && parsedWork.cli) {
+			parsedWork.exec = bone.cli(parsedWork.cli).cname;
+		}
+	}
+
+	if(parsedWork.exec) {
+		if(parsedWork.exec in tasks || parsedWork.exec in commanders) {
+			var options = [];
+			var command = [];
+			bone.utils.each(parsedWork.params, function(value, order) {
+				if(order === '_') {
+					if(!Array.isArray(value)) {
+						command.push(value);
+					} else {
+						command = command.concat(value);
+					}
+				} else {
+					options.push((order.length > 1 ? '--' : '-') + order);
+					if(value) {
+						options.push(value);
+					}
+				}
+			});
+			parsedWork.args = [parsedWork.exec].concat(options).concat(command);
+		} else {
+			parsedWork.inBone = false;
+			parsedWork.cmd = parsedWork.exec.split(' ')[0];
+			parsedWork.args = parsedWork.exec.split(' ').splice(1);
+		}
+	}
+
+	if(parsedWork.always) {
+		parsedWork.beforeRun = parsedWork.always;
+	}
+
+	return parsedWork;
+};
+function Task(name, raw) {
+	this.name = name;
+	this.raw = raw;
+	this.at = 0;
+	this.isMaster = process.env.BONE_TASK ? false : true;
+}
+
+Task.prototype.runAt = function(index) {
+	var work = this.raw[index];
+	var self = this;
+
+	if(!work) {
+		return;
+	}
+
+	work = _parse(work);
+
+	if(this.isMaster) {
+		_cp(work.inBone ? 'bone' : work.cmd, {
+			args: work.args,
+			env: {
+				BONE_TASK: this.name,
+				BONE_TASK_INDEX: index
+			}
+		}, function() {
+			self.runNext();
+		});
+	} else {
+		debug('run in child process.');
+		if(work.beforeRun) {
+			work.beforeRun();
+		}
+		if(work.exec) {
+			commander.parse(['node', 'bone'].concat(work.args));
+		}
+	}
+};
+
+Task.prototype.runNext = function() {
+	this.runAt(this.at++);
+};
+
+Task.prototype.run = function() {
+	this.runNext();
+};
+
+Task.tasks = {};
+
+exports.setup = function(b, cmder) {
+	bone = b;
+	commander = cmder;
+	/**
+		usage:
+			bone.cli.task('release', {
+				name: 'build',
+				params: {
+					project: 'dist'
+				}
+			},
+			{
+				name: 'connect'
+			},
+			{
+				exec: 'node main.js'
+			});
+	 */
+	bone.task = bone.cli.task = function(name) {
+		if(typeof name !== 'string') {
+			throw new Error('Fatal error: Task name must be set.');
+		}
+		if(name in Task.tasks) {
+			throw new Error('Override error: exist task.');
+		}
+		var list = Array.prototype.slice.call(arguments, 1);
+
+		Task.tasks[name] = new Task(name, list);
+
+		var usage = ['[options] [command]\r\n\r\n  Task:'];
+		for(var i in Task.tasks) {
+			usage.push(i);
+		}
+		commander.usage(usage.join(' '));
+	};
+};
+
+exports.run = function(task) {
+	if(task in Task.tasks) {
+		Task.tasks[task].run();
+		return true;
+	} else {
+		return false;
+	}
+};
+
+exports.runCP = function() {
+	var task = process.env.BONE_TASK;
+	var index = process.env.BONE_TASK_INDEX;
+
+	Task.tasks[task].runAt(index);
+};
